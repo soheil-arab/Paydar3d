b23379d6cf3895f240f49ae2f85d57079285b357
Rfun <erfan.tavakoli71@gmail.com>
2014-02-28 01:43:25 +0330

    assign lines

--- src/Localize.cpp
+++ src/Localize.cpp
@@ -60,15 +60,56 @@ Localize::Localize(WorldModel *wm)
     line_global["PL"]=line(Vector3f(-13.2,-1.95,0),Vector3f(-13.2,1.95,0));
     line_global["PLS"]=line(Vector3f(-15,1.95,0),Vector3f(-13.2,1.95,0));
 
+    flag_names.push_back("G1L");
+    flag_names.push_back("G2L");
+    flag_names.push_back("G1R");
+    flag_names.push_back("G2R");
+    flag_names.push_back("F1L");
+    flag_names.push_back("F2L");
+    flag_names.push_back("F1R");
+    flag_names.push_back("F2R");
+
+      line_names.push_back("FL");
+    line_names.push_back("FR");
+
+    line_names.push_back("FN");
+    line_names.push_back("FS");
+    line_names.push_back("PRN");
+    line_names.push_back("PR");
+    line_names.push_back("PRS");
+    line_names.push_back("PLN");
+    line_names.push_back("PL");
+    line_names.push_back("PLS");
+
+
+
+
+
 }
+
+
 
 void Localize::test()
 {
+    if(WM->getLastSeenLines().size()>0)
+    {
+
+        for(vector<line>::iterator l=WM->getLastSeenLines().begin();l!=WM->getLastSeenLines().end();l++)
+        {
+            cout<<"num of lines : "<<WM->getLastSeenLines().size()<<endl;
+            Vector3f beg = l->getBegin();
+            Vector3f end = l->getEnd();
+            beg = PolarToCartecian(beg);
+            end = PolarToCartecian(end);
+            line lcart=line(beg,end);
+            cout<<"beg : " << beg << "end : " <<end <<"---> "<<line_recognitation(lcart)<<endl;
+        }
+    }
 
 
-//    cout<<WM->getFlagGlobal()["F1R"]<<"soheile kalle kiri"<< endl;
-//    cout<<WM->getFlagGlobal()["G2L"]<<"soheili kalle tokhmi"<<endl;
-//    cout<<Distance_Cartesian_Point_Line(WM->getFlagGlobal()["F1R"],line_global["PRL"])<<endl;
+    //    cout<<WM->getFlagGlobal()["F1R"]<<"soheile kalle kiri"<< endl;
+    //    cout<<WM->getFlagGlobal()["G2L"]<<"soheili kalle tokhmi"<<endl;
+    //    cout<<Distance_Cartesian_Point_Line(WM->getFlagGlobal()["F1R"],line_global["PRL"])<<endl;
 }
 
 /*
@@ -105,43 +146,88 @@ double Localize::Distance_Cartesian_Point_Line(Vector3f point_polar,line l)
 }
 
 
-Vector3f Localize::calcPlaneNormal(vector<Vector3f> planePoints)
+
+
+//Vector3f Localize::calcPlaneNormal(vector<Vector3f> planePoints)
+//{
+//    if (planePoints.size() < 3) {
+//        return ;
+//    }
+
+//    double** vec = new double*[planePoints.size()];
+//    for(int i=0;i<planePoints.size();i++){
+//        vec[i] = new double[3];
+//        x += planePoints.at(i).x();
+//        y += planePoints.at(i).y();
+//        z += planePoints.at(i).z();
+//    }
+//    double x = 0, y = 0, z = 0;
+
+//    Vector3f cntPoint(x / points.size(), y / points.size(), z / points.size());
+
+//    for(int i=0;i<planePoints.size();i++){
+//        vec[i][0] = planePoints.at(i).x() - cntPoint.x();
+//        vec[i][1] = planePoints.at(i).y() - cntPoint.y();
+//        vec[i][2] = planePoints.at(i).z() - cntPoint.z();
+//    }
+
+//    // Compute SVD & Plane Normal
+//    SVD svd(vec,planePoints.size(), 3);
+//    double** V = svd.getV();
+//    Vector3f res = new Vector3f(V[0][2], V[1][2], V[2][2]);
+
+//    cntPoint.Normalize();
+
+//    if (centroid3d.dot(res) > 0) {
+//        res*=-1;
+//    }
+
+//    return res;
+
+//}
+
+
+
+
+string Localize::line_recognitation(line line_to_detect)
 {
-    if (planePoints.size() < 3) {
-        return ;
+    double min=1000;
+    string result="";
+
+
+    for(vector<string>::iterator i=flag_names.begin();i!=flag_names.end();i++){
+
+        if(WM->getFlagLastSeen(*i)==WM->serverTime)
+        {
+
+            cout<<"flag seen"<<endl;
+            for(vector<string> ::iterator j=line_names.begin();j!=line_names.end();j++)
+            {
+
+
+
+                if(fabs(Distance_Cartesian_Point_Line(WM->flag[*i],line_to_detect) - Distance_Cartesian_Point_Line(WM->flagGlobal[*i],line_global[*j]))<0.5)
+                {
+
+                    cout<<"flag name is:"<<*i<<endl;
+                    cout<<"line name is:"<<*j<<endl;
+                    cout<<Distance_Cartesian_Point_Line(WM->flag[*i],line_to_detect)<<"local distance"<<endl;
+
+                    cout<<Distance_Cartesian_Point_Line(WM->flagGlobal[*i],line_global[*j])<<"global distance"<<endl;
+                    cout<<"fabse kiri"<<fabs(Distance_Cartesian_Point_Line(WM->flag[*i],line_to_detect) - Distance_Cartesian_Point_Line(WM->flagGlobal[*i],line_global[*j]))<<endl;
+
+                    result = *j;
+                    return result;
+//                    cout<<"flag name:"<<*i<<"coordinate: "WM->flag[*i]<<"line name:"<<*j<<endl;
+//                    cout<<"min: "<<min<<endl;
+                }
+            }
+        }
     }
 
-    double** vec = new double*[planePoints.size()];
-    for(int i=0;i<planePoints.size();i++){
-        vec[i] = new double[3];
-        x += planePoints.at(i).x();
-        y += planePoints.at(i).y();
-        z += planePoints.at(i).z();
-    }
-    double x = 0, y = 0, z = 0;
-
-    Vector3f cntPoint(x / points.size(), y / points.size(), z / points.size());
-
-    for(int i=0;i<planePoints.size();i++){
-        vec[i][0] = planePoints.at(i).x() - cntPoint.x();
-        vec[i][1] = planePoints.at(i).y() - cntPoint.y();
-        vec[i][2] = planePoints.at(i).z() - cntPoint.z();
-    }
-
-    // Compute SVD & Plane Normal
-    SVD svd(vec,planePoints.size(), 3);
-    double** V = svd.getV();
-    Vector3f res = new Vector3f(V[0][2], V[1][2], V[2][2]);
-
-    cntPoint.Normalize();
-
-    if (centroid3d.dot(res) > 0) {
-        res*=-1;
-    }
-
-    return res;
-
+    return result;
 }
+
 
 
 
--- src/Localize.h
+++ src/Localize.h
@@ -2,7 +2,7 @@
 #define LOCALIZE_H
 #include "WorldModel.h"
 //#include "line.h"
-
+#include "Math.h"
 
 using namespace std;
 class Localize{
@@ -11,9 +11,12 @@ public:
     double Distance_Polar_Point_Line(Vector3f point_polar,line l);
     double Distance_Cartesian_Point_Line(Vector3f point_polar,line l);
     Vector3f calcPlaneNormal(vector<Vector3f> planePoints);
+    string line_recognitation(line line_to_detect);
 
     void test();
     WorldModel *WM;
     map<string,line> line_global;
+    vector<string> flag_names;
+    vector<string> line_names;
 };
 #endif
